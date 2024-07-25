33c0f4dfa9d65d128caa6fe83da28c4c6d613c65
rachidfrex <2003043000250@ofppt-edu.ma>
2024-07-25 17:25:19 +0100

    Update header and nav components to improve layout and styling

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Spotlight } from "../components/ui/Spotlight";
 function Header() {
   return (
-    <div className="flex flex-col justify-center items-center  w-full  h-[80vh] ">
+    <div className="flex flex-col justify-center items-center  w-full  h-[40vh] md:h-[80vh] ">
       <Spotlight
-        className="-top-40  left-0 md:left-60 md:-top-20"
+        className=" md:ml-20 -top-40  left-0 md:left-60 md:-top-20"
         fill="white"
       />
 
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -9,8 +9,7 @@ function Nav() {
             <nav className=' text-center w-full py-1  md:w-auto  text-sm bg-zinc-800/90 gap-1 px-1  flex  text-white     rounded-full    '>
 
             <img src={logo} className='h-7 w-7 mx-2  ' alt="" />
-            <span></span>
-            <ul className='flex font-rubik  justify-center items-center space-x-4 '>
+            <ul className='flex font-rubik w-full justify-center items-center space-x-4 '>
             <li className=' bg-zinc-700 px-3 py-0.5 rounded-full'>About</li>
             <li  className=' px-3 py-1 rounded-full'>project </li>
             <li  className=' px-3 py-1 rounded-full'>Contact</li>
