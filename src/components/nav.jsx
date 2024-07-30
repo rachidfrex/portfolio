1f8f7e54e4a95888e38a0ad223627dd81ee1921c
rachidfrex <2003043000250@ofppt-edu.ma>
2024-07-30 18:11:46 +0100

    chore: Update Nav component layout and styling

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -7,7 +7,7 @@ function Nav() {
   const [selected, setSelected] = useState('about'); 
 
   return (
-    <div className=' fixed w-full z-50 flex justify-center mx-3 items-center py-5'>
+    <div className=' fixed w-full z-50 flex mt-4 justify-center  items-center '>
       <nav className='text-center py-1 w-auto text-sm overflow-hidden bg-zinc-800/90 gap-1 px-1 flex text-white rounded-full'>
         <img src={logo} className='h-7 w-7 mx-2' alt="" />
         <ul className='flex font-rubik w-full justify-center items-center space-x-4'>
@@ -17,6 +17,7 @@ function Nav() {
               {selected === item && (
                 <motion.div
                   layoutId="underline"
+
                   className="bg-zinc-700  absolute bottom-0 -z-[1] rounded-full  left-0 right-0 h-full"
                   initial={false}
                   transition={{ type: 'spring', stiffness: 500, damping: 30 }}
