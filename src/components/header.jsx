a321e29e1964179df286764b33102a8c0c533b05
rachidfrex <2003043000250@ofppt-edu.ma>
2024-07-31 17:10:23 +0100

    chore: Update header component with fixed height for name text

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -15,7 +15,7 @@ function Header() {
       
     
       <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
-        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+        <h1 className="text-4xl md:text-7xl h-[155px] font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
           Hi my Name is <br />  Rachid EL Hajouji
         </h1>
         <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
